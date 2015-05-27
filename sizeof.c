9fa04a756fcfba0054ffd2801d175647b3a234bc
Aymeric RATINAUD <aratinau@e1r8p4.42.fr>
2015-05-27 09:56:33 +0200

    curiosite de wells a satisfaire

--- sizeof.c
+++ sizeof.c
@@ -2,8 +2,19 @@
 
 int		main()
 {
-	printf("int : %i\n", sizeof(int));
-	printf("long int : %i\n", sizeof(long int));
-	printf("long long int : %i\n", sizeof(long long int));
+	/*
+	 * Bonjour Mosieur Wells, apres une premiere tentative de compilation a 
+	 * l'ecole le compilateur m'a indique une erreur dans le printf car
+	 * l'argument %i ne peut afficher les unsigned long
+	 * et m'a dirige vers l'argument %lu ce qui donne le resultat suivant :
+	 * int : 4
+	 * long int : 8
+	 * long long int : 8
+	 *
+	 * En esperant avoir repondu a votre requete
+	 * */
+	printf("int : %lu\n", sizeof(int));
+	printf("long int : %lu\n", sizeof(long int));
+	printf("long long int : %lu\n", sizeof(long long int));
 	return (0);
 }
